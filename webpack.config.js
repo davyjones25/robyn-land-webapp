84c52a17bfa7527b5edbbbe109c65e9dfb4b581e
code salley <codesalley@gmail.com>
2023-01-10 02:24:36 +0000

    change webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,19 @@
 const path    = require("path")
 const webpack = require("webpack")
 
+require( '@babel/preset-env')
+
 module.exports = {
   mode: "production",
   devtool: "source-map",
   entry: {
-    application: "./app/javascript/application.js"
+    application: "./app/javascript/application.js",
   },
   output: {
     filename: "[name].js",
     sourceMapFilename: "[file].map",
     path: path.resolve(__dirname, "app/assets/builds"),
-  }, 
+  },
   plugins: [
     new webpack.optimize.LimitChunkCountPlugin({
       maxChunks: 1
@@ -20,17 +22,13 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.m?js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
-          }
-        }
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use:["babel-loader"]
       }
     ]
+  },
+  resolve: {
+    extensions: ["*", ".js", ".jsx"]
   }
 }
-
-
