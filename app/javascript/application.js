06b659ade2c2e5d87321b231fbd8e4dd3c3f78ea
code salley <codesalley@gmail.com>
2023-01-10 02:25:20 +0000

    react rails with webpack

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,11 +1,13 @@
 // Entry point for the build script in your package.json
-import { Application } from "@hotwired/stimulus"
-import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers"
+// import { Application } from "@hotwired/stimulus"
+// import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers"
 
-window.Stimulus = Application.start()
-const context = require.context("./controllers", true, /\.js$/)
-Stimulus.load(definitionsFromContext(context))
+import "@hotwired/turbo-rails"
 
-import "./controllers";
+require("./controllers")
+
+
+
 import "bootstrap";
 import "@fortawesome/fontawesome-free/js/all";
+
